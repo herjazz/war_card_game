197371432d99f6f1838cf529705f1b717a42675d
Franco <herjaxx@yahoo.co.uk>
2023-02-13 08:55:45 +0900

    Added type hints; linting

--- war_game.py
+++ war_game.py
@@ -27,7 +27,7 @@ class Player:
     Attributes:
         name (str): name of player
         deck (list[pc.Card]): deck of cards -> class Card from playing_cards
-        discarded (list[none]): empty list for discarded cards
+        discarded (list[pc.Card]): empty list for discarded cards
     """
 
     def __init__(self, name: str, deck: list[pc.Card] = None):
@@ -37,6 +37,7 @@ class Player:
         Args:
            name (str): name of player
            deck (list[pc.Card]): deck of cards
+           discarded (list[pc.Card]): empty list for discarded cards
         """
 
         self.name = name
@@ -64,15 +65,15 @@ class Player:
     def add_cards(self, cards: list[pc.Card]):
         """
         Adds cards to start of deck
-        
+
         Args:
             cards (list[pc.Card]): list of cards to add
         """
-        
+
         self.deck = cards + self.deck
 
     def __str__(self) -> str:
-        """ Prints number of cards left in self.deck """
+        """Prints number of cards left in self.deck"""
         return f"{self.name} has {len(self.deck)} card(s) left."
 
     def __repr__(self) -> str:
@@ -83,7 +84,7 @@ class Player:
 
 
 def choose_num_players() -> int:
-    """ Function to choose number of players; exits after 5 failed attempts """
+    """Function to choose number of players; exits after 5 failed attempts"""
     attempts: int = 0
     while attempts < 5:
         num_players = input("How many players? (Choose 2, 3, or 4): ")
@@ -95,8 +96,8 @@ def choose_num_players() -> int:
     sys.exit("Too many failed attempts. Exiting.")
 
 
-def create_players(deck, num_players: int) -> list[Player]:
-    """ Create a list of players with decks """
+def create_players(deck: list[pc.Card], num_players: int) -> list[Player]:
+    """Create a list of players with decks"""
     random.shuffle(deck)
     deck_size: int = len(deck) // num_players
     decks = (deck[i : i + deck_size] for i in range(0, len(deck), deck_size))
@@ -104,12 +105,12 @@ def create_players(deck, num_players: int) -> list[Player]:
 
 
 def num_winners(scores: list[int], top_score: int) -> int:
-    """ Return the number of values that match a top score """
+    """Return the number of values that match a top score"""
     return sum(1 for v in scores.values() if v == top_score)
 
 
 def check_winner(scores_dict: dict[Player, int], war: bool = False) -> None:
-    """ Print name of winner and update their deck with won cards """
+    """Print name of winner and update their deck with won cards"""
     winnings: list[pc.Card] = []
     winner: Player = max(scores_dict, key=scores_dict.get)
     print(f"{winner.name} wins the hand!")
@@ -129,7 +130,7 @@ def main():
 
     while num_rounds < 10:
         for player in players:
-            card = player.play_hand()
+            card: pc.Card = player.play_hand()
             round_scores[player] = war_scores[card.rank]
         max_score: int = max(round_scores.values())
         war_play: bool = False
@@ -137,12 +138,12 @@ def main():
         while num_winners(round_scores, max_score) > 1:
             war_play = True
             print("WAR!")
-            remaining_players = []
+            remaining_players: list[Player] = []
             for pl, score in round_scores.items():
                 if score == max_score:
                     remaining_players.append(pl)
                 else:
-                    # To ensure "loser" doesn't have highest score if war cards end up lower
+                    # Reset non-playing player's score
                     round_scores[pl] = 0
             for player in remaining_players:
                 if len(player.deck) == 1:
@@ -174,16 +175,20 @@ def main():
         num_rounds += 1
 
     # Find player with largest deck
-    largest_deck_size = max([len(pl.deck) for pl in players])
-    biggest_decks = [pl for pl in players if len(pl.deck) == largest_deck_size]
+    largest_deck_size: int = max([len(pl.deck) for pl in players])
+    biggest_decks: list[Player] = [
+        pl for pl in players if len(pl.deck) == largest_deck_size
+    ]
     print("Maximum number of rounds exceeded!")
     if len(biggest_decks) == 1:
         print(f"{biggest_decks[0].name} has the most cards - they are the winner!")
     else:
         # Tiebreak
         # TODO: deal with a 3-way tiebreak
-        print(f"TIEBREAK!!! between {biggest_decks[0].name} and {biggest_decks[1].name}!")
-        cards = []
+        print(
+            f"TIEBREAK!!! between {biggest_decks[0].name} and {biggest_decks[1].name}!"
+        )
+        cards: list[int] = []
         for pl in biggest_decks:
             card = pl.play_hand()
             cards.append(war_scores[card.rank])
@@ -191,8 +196,9 @@ def main():
             # TODO: Add code to deal with same cards
             print("Try again")
         else:
-            winner = cards.index(max(cards))
+            winner: int = cards.index(max(cards))
             print(f"{biggest_decks[winner].name} wins via tiebreak!")
+
 
 if __name__ == "__main__":
     main()
