e169ef1636fd79291341881e56282c82a1ca6645
Franco <herjaxx@yahoo.co.uk>
2023-02-14 08:17:28 +0900

    Added docstrings & type hints to playing.cards.py

--- playing_cards.py
+++ playing_cards.py
@@ -14,42 +14,28 @@ class Card:
 
     """
     Card represents a single playing card
+
+    Attributes:
+        rank (str): rank of card
+        suit (str): suit of card
     """
 
     def __init__(self, rank: str, suit: str):
+        """
+        Initializes class attributes
+
+        Args:
+           rank (str): rank of card
+           suit (str): suit of card
+        """
+
         self.rank = rank
         self.suit = suit
 
     def __repr__(self) -> str:
+        """ Represents an instance of Card with rank then suit """
         return f'{self.rank} of {self.suit}'
 
-def create_deck():
+def create_deck() -> list[Card]:
+    """ Creates an unshuffled standard deck of cards as a list """
     return [Card(rank, suit.value) for rank in ranks for suit in Suit]
-
-# class CardVal:
-#     def __init__(self, value: int, suit: str):
-#         self.value = value
-#         self.suit = suit
-
-#     def __repr__(self) -> str:
-#         match self.value:
-#             case 1 | 14:
-#                 rank = "A"
-#             case 11:
-#                 rank = "J"
-#             case 12:
-#                 rank = "Q"
-#             case 13:
-#                 rank = "K"
-#             case _:
-#                 rank = self.value
-#         match self.suit:
-#             case "spades":
-#                 suit = chr(9824) # Character 9824 is '♠'.
-#             case "clubs":
-#                 suit = chr(9827) # Character 9827 is '♣'.
-#             case "hearts":
-#                 suit = chr(9829) # Character 9829 is '♥'.
-#             case "diamonds":
-#                 suit = chr(9830) # Character 9830 is '♦'.
-#         return f'{rank} of {suit}'
