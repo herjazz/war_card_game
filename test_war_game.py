8f90224b68ba6a4f4d3eaef530e031f5ecc8ae49
Franco <herjaxx@yahoo.co.uk>
2023-02-18 15:03:55 +0900

    Added tests for WarPlayer class methods.

---
+++ test_war_game.py
@@ -0,0 +1,26 @@
+import unittest
+import playing_cards as pc
+from war_game import WarPlayer
+
+class TestWarPlayer(unittest.TestCase):
+    """ Tests for the class WarPlayer """
+
+    def setUp(self):
+        """ Create common items for test methods """
+        deck = pc.create_deck()
+        self.cards = deck[5:10]
+        self.player = WarPlayer('Test Player', deck)
+
+    def test_play_hand_discarded(self):
+        """ Check card gets added to discard pile """
+        card = self.player.play_hand()
+        self.assertIn(card, self.player.discarded)
+
+    def test_add_cards(self):
+        """ Check card gets added to discard pile """
+        self.player.add_cards(self.cards)
+        self.assertEqual(self.cards, self.player.deck[:5])
+
+
+if __name__  == '__main__':
+    unittest.main()
